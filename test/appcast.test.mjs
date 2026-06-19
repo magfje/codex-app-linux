ec2158fe30bccfe150abfe208719a08b9cebbba9
cau1k <zero@composer.group>
2026-06-19 12:29:14 -0400

    build: bump package revision to 31

--- test/appcast.test.mjs
+++ test/appcast.test.mjs
@@ -36,10 +36,10 @@ test("npmVersionFor applies launcher revision suffixes", () => {
     buildNumber: "1041"
   };
 
-  assert.equal(npmVersionFor("prod", upstream), "26.313.41514-launcher.30");
+  assert.equal(npmVersionFor("prod", upstream), "26.313.41514-launcher.31");
   assert.equal(
     npmVersionFor("beta", upstream),
-    "26.313.41514-beta.1041.launcher.30"
+    "26.313.41514-beta.1041.launcher.31"
   );
   assert.equal(npmVersionFor("prod", upstream, 0), "26.313.41514");
   assert.equal(npmVersionFor("beta", upstream, 0), "26.313.41514-beta.1041");
